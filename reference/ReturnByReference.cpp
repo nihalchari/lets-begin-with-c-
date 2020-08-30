7c78ac89dd6ac7c2f0c439578660d955176a272b
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-08-30 22:21:59 +0530

    session 1-2 and reference

---
+++ reference/ReturnByReference.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int & returnByRef( int &x);
+int & riskReturnByRef(int &z);
+
+int main(void)
+{   
+    int a(5);
+    int z=20;                   
+
+    std::cout << "&a =" << &a << std ::endl;
+
+    auto & k = returnByRef(a);
+
+    std::cout << "&k= " << &k <<  std::endl;
+    std::cout << "a= " << a <<  std::endl;          //6
+
+    ///@note some strange behaviour
+    // returnByRef(a) = z;
+    // std::cout << "a= " << a <<  std::endl;       //20
+
+    // riskReturnByRef(a)=55;
+    // std::cout << "a= " << a <<  std::endl;
+
+    return 0;
+}
+
+///@note just for demo
+int & returnByRef(int &x)
+{
+    std::cout << "&x =" << &x << std ::endl;
+    ++x;
+    return x;
+}
+
+
+///@note ou should return a reference to an existing object that isn't going away immediately, 
+///and where you don't intend any transfer of ownership.
+
+// int & riskReturnByRef(int &z)
+// {
+//     auto t = z;
+//     ++t;
+//     ///@note should not return reference to local var
+//     return t;
+// }

9adf56ac3e2f5b9838d318d54489c2eb4e7aa9b1
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-09-04 17:19:04 +0530

    Namespace

---
+++ namespace/Namespace.cpp
@@ -0,0 +1,59 @@
+#include "cstdio"
+#include<iostream>
+
+
+///@brief
+/// no namespace inside function
+/// main() should be global namespace always
+
+
+///@note programm scope
+int i = 10;
+
+
+///@note namespace scope
+namespace name1
+{
+    int i =2020;
+    
+    namespace name11
+    {
+        int i=2021;
+    }
+}
+
+namespace name2
+{
+    int i2 =2022;
+
+}
+
+
+int main()
+{
+    using namespace name2;
+
+    ///@note this will access global value
+    printf("::i = %d\n", ::i);  
+
+    ///@note accessing value inside namespace
+    printf("name1::name11::i = %d\n", name1::name11::i);
+
+    ///@note since we have used "using namespace name2"
+    printf("name2::i2 = %d\n", i2); 
+    
+
+
+    {
+        ///block scope
+        int i = 50;  
+
+        ///@note this will access global value
+        printf("::i = %d\n", ::i);
+        // printf("i = %d\n", i);  
+      
+    }
+
+    
+    return 0;
+}

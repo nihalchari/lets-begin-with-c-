993f19da529310cf2321030550bbe3d39b814250
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-09-17 16:59:18 +0530

    Hello world

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Chiranjeevi Maddi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num {10};
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    cout << "Enter a number between 1 to 100" << endl; // prints !!!Hello World!!!
+	cin >> num;
+	cout << "This is my favourite number";
+	return 0;
+}

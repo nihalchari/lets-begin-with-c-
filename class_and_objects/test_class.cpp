ae6657664c0b05b7cac4877be7d2975139b8ab4b
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-09-25 18:05:16 +0530

    acess specifier and this*

--- class_and_objects/test_class.cpp
+++ class_and_objects/test_class.cpp
@@ -5,17 +5,21 @@ class Complex
 {
     ///@note access specifier can be public or private or protected
     /// for class by default members are private, for structure by default is public
-    
-    public:
-    //data members or fields or attributes
-    int id;
-    char name[20];
   
+    public:
     //member functions or methods or behaviours
     void getData()
     {
+        if(validateData())
+        {
             cout << "id :" << id << endl; 
             cout << "name :" << name << endl;
+        }
+        else
+        {
+            cout << "no valid data available" << endl;
+        }
+        
     }
 
     void setData()
@@ -24,7 +28,6 @@ class Complex
         cin >> id;
         cout << "Enter name" << endl;
         cin >> name;
-
     }
 
     private:
@@ -34,6 +37,11 @@ class Complex
         cout << "Validating data...." << endl;
         return true;
     }
+
+    private:
+    //data members or fields or attributes
+    int id;
+    char name[20];
 };
 
 class EmptyClass
@@ -45,17 +53,16 @@ int main()
     /// object or instance
     Complex testObj;
 
-    /// size of object will be size of data members of class
-    cout << "size testClass obj: " << sizeof(testObj) << endl;
+    // /// size of object will be size of data members of class
+    // cout << "size testClass obj: " << sizeof(testObj) << endl;
 
     EmptyClass emptyObj;
-    ///size of empty class is 1 byte
-    cout << "size emptyClass obj: " << sizeof(emptyObj) << endl << endl;
+    // ///size of empty class is 1 byte
+    // cout << "size emptyClass obj: " << sizeof(emptyObj) << endl << endl;
 
     /// accesing data members from object
     testObj.setData();
     testObj.getData();
-    testObj.id = 100;
     testObj.getData();
 }
 
--- class_and_objects/this_ptr.cpp
+++ class_and_objects/this_ptr.cpp
@@ -10,50 +10,65 @@ using namespace std;
 
 ///signature of this pointer : classname * const this
 
-
 class Complex
 {
-    public:   
+    private:   
      int real;
      int img;
 
     ///TestClass * const this
+   
+    public:
     void getAddress()
     {
        std::cout <<   "inside member function    :" << this << endl; 
     }
 
-    // void initComplex(int _real, int _img)
-    // {
-    //     this->img = _img;
-    //     real=_real;   
-    // }
+    void initComplex(int _real, int _img)
+    {
+        this->img = _img;
+        real=_real;   
+    }
 
-    // void printComplex()
-    // {
-    //     cout <<"this->real :" << this->real << endl;
-    //     cout <<"img       :" << img << endl;
-    // }
+    void printComplex()
+    {
+        cout <<"this->real :" << this->real << endl;
+        cout <<"img       :" << img << endl;
+    }
+
+     Complex sum(Complex other)
+     {
+         Complex temp;
+         temp.img = this->img+other.img;
+         temp.real = this->real+other.real;
+         return temp;
+     }
+
 };
 
 int main()
 {
-    Complex testObj1;
+    Complex testObj1;   
     Complex testObj2;
 
     cout << "for testObj1" << endl;
     std::cout << "inside main               :" << &testObj1 << endl;
     testObj1.getAddress();
-    // cout << "\n\nfor testObj1" <<endl;
-    // std::cout << "inside main               :" << &testObj2 << endl;
-    // testObj2.getAddress();
+    cout << "\n\nfor testObj1" <<endl;
+    std::cout << "inside main               :" << &testObj2 << endl;
+    testObj2.getAddress();
 
-    // testObj1.initComplex(10,100);   
-    // testObj2.initComplex(20,200);
-    // testObj1.printComplex();
-    // testObj2.printComplex();
+    testObj1.initComplex(10,100);   
+    testObj2.initComplex(20,200);
 
-    // Complex C = testObj1 + testObj2;    //30, 300
+    testObj1.printComplex();
+
+    Complex res = testObj1.sum(testObj2);
+
+    res.printComplex();
+
+    ///@note not possible as real and img both are private
+    // Complex C.real = testObj1.real + testObj2.real;    //30, 300
 
     return 0;
 }

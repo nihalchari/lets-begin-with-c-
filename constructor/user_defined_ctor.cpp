4436e6c644df9100e773af2392c7fa2418ffb1ff
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-11-23 20:33:42 +0530

    cleaned constructor
    Signed-off-by: Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>

--- constructor/user_defined_ctor.cpp
+++ constructor/user_defined_ctor.cpp
@@ -9,59 +9,50 @@ class TestClass
 {
     public:
     ///@note initialized using ctor initializer list
-    TestClass():m_iNum(100),m_fNum(10.10f)
+    TestClass():m_fNum(10.10f),m_iNum(1)
     {   
     cout << "TestClass ctor parameterless"<<endl;
     };
 
-    // TestClass(int val):m_iNum(val), m_fNum(10.10f)
-    // {   
-    //     cout << "TestClass ctor with param int"<<endl;
-    // };
+    TestClass(int val):m_iNum(val), m_fNum(20.22f)
+    {   
+        cout << "TestClass ctor with param int"<<endl;
+    };
 
-    // TestClass(float val):m_fNum(val), m_iNum(100)
-    // {   
-    //     cout << "TestClass ctor with param float"<<endl;
-    // };
+    TestClass(float val):m_fNum(val), m_iNum(35)
+    {   
+        cout << "TestClass ctor with param float"<<endl;
+    };
     
     // ///@note initialized using ctor initializer list
-    // TestClass(float fval, int ival):m_iNum(ival),m_fNum(fval)
-    // {  
-    //     cout << "TestClass ctor with param float and int"<<endl;
-    // };
-
+    TestClass(float fval, int ival):m_iNum(ival),m_fNum(fval)
+    {  
+        cout << "TestClass ctor with param float and int"<<endl;
+    };
 
     int m_iNum;
     float m_fNum;
 };
 
 
-
 int main()
 {    
 
-    // TestClass obj1;
-    // cout << obj1.m_iNum << endl;
-    // cout << obj1.m_fNum << endl;
+    TestClass obj1;
+    cout << obj1.m_iNum << endl;
+    cout << obj1.m_fNum << endl;
 
+    TestClass obj2(20);
+    cout << obj2.m_iNum << endl;
+    cout << obj2.m_fNum << endl;
 
-    //  TestClass obj2{5.5f};
-    // cout << obj2.m_iNum << endl;
-    // cout << obj2.m_fNum << endl;
+    TestClass obj3{33.30f};
+    cout << obj3.m_iNum << endl;
+    cout << obj3.m_fNum << endl;
 
-    // TestClass obj3{6};
-    // cout << obj3.m_fNum << endl;
-    // cout << obj3.m_iNum << endl;
-    
-
-    // TestClass obj4{222.2f, 888};
-    // cout << obj4.m_fNum << endl;
-    // cout << obj4.m_iNum << endl;
-
-    
-    // auto obj = TestClass();
-    // cout << obj.m_iNum << endl;
-    // cout << obj.m_fNum << endl;
+    TestClass obj4{40.40f, 40};
+    cout << obj4.m_fNum << endl;
+    cout << obj4.m_iNum << endl;
 
     return 0;
 }

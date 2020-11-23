ca493d100d22e291ba03fae0c755f46c372456ab
Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>
2020-11-23 20:35:12 +0530

    cleaned string
    Signed-off-by: Chari Nehal Dattaram (RBEI/EBB1) <NehalDattaram.Chari@in.bosch.com>

--- class_and_objects/string.cpp
+++ class_and_objects/string.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    auto s1 = string("String1");
+    auto s1 =string("String1");
     string s2{"String2"};
 
     
@@ -15,24 +15,31 @@ int main()
     auto s = s1+s2;
     cout << "s : " << s << endl;
 
-    // auto s4 = s1.append(s2);
-    // cout << "s4 : " << s4 << endl;
+    auto s4 = s1.append(s2);
+    cout << "s4 : " << s4 << endl;
 
-    // auto c1 = s1.at(0);
-    // auto c2 = s1.at(1);
-    // auto c3 = s1.at(2);
-    // cout << c1 << c2 << c3 << endl;
+    s1.push_back('2');
+    cout << s1 << endl;
 
-    // auto it = s2.end()-1;
-    // cout << *it <<endl;
+    s1.pop_back();
+    s1.pop_back();
+    cout << s1 << endl;
 
-    // auto ptr = s1.c_str();
+    auto c1 = s1.at(0);
+    auto c2 = s1.at(1);
+    auto c3 = s1.at(2);
+    cout << c1 << c2 << c3 << endl;
 
-    // s1.clear();
-    // cout << s1 << endl;
+    auto it = s2.end()-1;
+    cout << *it <<endl;
 
-    // auto found = s1.find('i');
-    // cout << found << endl;
+     auto ptr = s1.c_str();
+
+    s1.clear();
+    cout << s1 << endl;
+
+    auto found = s1.find('i');
+    cout << found << endl;
 
     return 0;
 } 
